fc0825c66fc52d4a9fbdda2077c56640263eb84f
Vries <j.devries4@uu.nl>
2020-11-16 15:05:05 +0100

    initial test

---
+++ src/test.R
@@ -0,0 +1,47 @@
+## ---------------------------
+#'
+#' Script name: Plot offshore boundary results
+#'
+#' Short Description: 
+#' 
+#'
+#' Author: Job de Vries
+#'
+#' Date Created: 2020-11-16
+#'
+#' Copyright (c) Job de Vries, 2020
+#' Email: j.devries4@uu.nl
+#'
+## ---------------------------
+#'
+#' Description
+#' 
+#'
+#'
+## ---------------------------
+
+rm(list = ls())
+#' set working directory for Mac and PC
+wd<-getwd()
+
+setwd("D:/BackUp_D_mangroMud_202001/Research/Software/Projects/offshore_boundary")
+
+
+## ---------------------------
+
+options(scipen = 6, digits = 4) # I prefer to view outputs in non-scientific notation
+memory.limit(30000000)     # this is needed on some PCs to increase memory allowance, but has no impact on macs.
+
+## ---------------------------
+
+#' load up the packages 
+source("./src/packages.R")       # loads up all the packages we need
+
+## ---------------------------
+source("./src/functions.R")
+
+## ---------------------------
+
+dataFolder <- './data/raw'
+
+# transect  <- readOGR(paste0(dataFolder, '/transects'), '2009_WnZ_transect')
